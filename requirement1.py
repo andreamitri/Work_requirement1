71da6b153699731bff9b1bc8a2092eeac4dd7053
andreamitri <ladanandrea@gmail.com>
2026-02-11 10:30:56 +0100

    Exercise 2:

--- requirement1.py
+++ requirement1.py
@@ -8,3 +8,18 @@ if age >= 18:
 else:
     print(f"Sorry {name}, you are too young to enter.")
     
+
+# Exercise 2: Number List Processor
+
+n = int(input("\nEnter a number: "))
+
+numbers = []
+for i in range(1, n + 1):
+    numbers.append(i)
+
+print(numbers)
+
+if n > 5:
+    print("The list is long.")
+else:
+    print("The list is short.")
